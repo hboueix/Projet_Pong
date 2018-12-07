6915a572efa195877878e693520e9e6d345b7a18
Hugo Boueix <hugo.boueix@ynov.com>
2018-12-07 17:02:33 +0100

    Image balle

--- pong.py
+++ pong.py
@@ -8,13 +8,12 @@ hauteur = 1080
 fenetre = Tk()
 fenetre.title('Pong')
 canvas = Canvas(fenetre, width = largeur, height = hauteur, bg = 'white')
-img_balle = PhotoImage(file = "./Ressources/balle.png")
-balle = canvas.create_image(largeur/0, hauteur/0, image = img_balle)
-
+img_balle = PhotoImage(file = "Projet_Pong/Ressources/balle.png")
+balle = canvas.create_image(0, 0, anchor = NW, image = img_balle)
 
 # Placement des composants
 canvas.grid(row = 0, column = 0, columnspan = 2)
-canvas.create_image(largeur/0, hauteur/0, image = img_balle)
+
 
 # Boucle infinie
 fenetre.mainloop()
