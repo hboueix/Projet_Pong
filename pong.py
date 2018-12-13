6ffffd80053ad2414ba34df9fe4daab934b80021
Hugo Boueix <hugo.boueix@ynov.com>
2018-12-13 02:19:42 +0100

    Ajout des raquettes, pas encore de contrôle dessus

--- pong.py
+++ pong.py
@@ -12,18 +12,24 @@ def ouvrir_menu():
 
 def start_pong():
     global canvas, ligne, balle
-    canvas, ligne, balle = ouvrir_canvas()
-    gameTick()
+    canvas, ligne, balle, raq1, raq2 = ouvrir_canvas()
+    canvas.bind_all('<Down>', move_raq_bas)
+    canvas.bind_all('<Up>', move_raq_haut)
+    canvas.bind_all('x', move_raq_bas)
+    canvas.bind_all('z', move_raq_haut)
+    move_balle()
 
 def ouvrir_canvas():
     menu.destroy()
     canvas = Canvas(fenetre, width = largeur, height = hauteur, bg = 'black')
     ligne = canvas.create_line(largeur / 2, 0, largeur /2, hauteur, fill = 'white', dash = (4,2,4,4))
     balle = canvas.create_oval((largeur / 2 - 10, hauteur / 2 - 10), (largeur / 2 + 10, hauteur / 2 + 10), fill = 'white')
+    raq1 = canvas.create_rectangle(20, (hauteur / 2 - hauteur /10), 40, (hauteur / 2 + hauteur / 10), fill = 'white')
+    raq2 = canvas.create_rectangle(largeur - 40, (hauteur / 2 - hauteur /10), largeur - 20, (hauteur / 2 + hauteur / 10), fill = 'white')
     canvas.pack()
-    return canvas, ligne, balle
+    return canvas, ligne, balle, raq1, raq2
 
-def gameTick():
+def move_balle():
     global dx, dy
     coords = canvas.coords(balle) # coords = (x1, y1, x2, y2)
     if coords[0] <= 0 or coords[2] >= largeur:
@@ -31,7 +37,17 @@ def gameTick():
     if coords[1] <= 0 or coords[3] >= hauteur:
         dy *= -1
     canvas.move(balle, dx, dy)
-    fenetre.after(5, gameTick) 
+    fenetre.after(5, move_balle) 
+
+def move_raq_bas(event):
+    global raq1
+    canvas.move(raq1, 0, 10)
+    print('down')
+
+def move_raq_haut(event):
+    global raq1
+    canvas.move(raq1, 0, -10)
+    print('up')
 
 # Fenêtre principale
 fenetre = Tk()
@@ -39,13 +55,14 @@ fenetre.title('Pong')
 fenetre.attributes('-fullscreen', 1)
 
 # Création des variables
-canvas, ligne, balle = None, None, None
+canvas, ligne, balle, raq1, raq2 = 0, 0, 0, 0, 0
 largeur = fenetre.winfo_screenwidth()
 hauteur = fenetre.winfo_screenheight()
 dx, dy = 3, 3
 
 # Appel des fonctions
 menu = ouvrir_menu()
+
 
 # Boucle infinie
 fenetre.mainloop()
