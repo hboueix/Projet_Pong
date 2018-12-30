cc95279eefa713a192dca4ed3382506bc4104875
Hugo Boueix <hugo.boueix@ynov.com>
2018-12-30 21:23:29 +0100

    Ecran parametres avec points OK et vitesse OK

--- pong_classe.py
+++ pong_classe.py
@@ -7,9 +7,25 @@ class MenuPrincipal: # lancement, /start_pong
         self.menu = Frame(fenetre, borderwidth = 2, relief = GROOVE, bg = "white")
         self.menu.pack(expand = TRUE)
         Label(self.menu, text = 'Menu principal', font = '-size 50', bg = 'white').pack(padx = 100, pady = 50)
-        Button(self.menu, text = 'Start', command = start_pong, font = '-size 22').pack(ipadx = 20, pady = 20)
+        Button(self.menu, text = 'Start', command = parametrage, font = '-size 22').pack(ipadx = 20, pady = 20)
         Button(self.menu, text = 'Quitter', command = fenetre.destroy, font = '-size 22').pack(ipadx = 20, pady = 20)
 
+class EcranParametre:
+
+    def __init__(self):
+        var_win, var_vit = StringVar(), StringVar()
+        var_win.set(3)
+        var_vit.set(3)
+        self.ecran = Frame(fenetre, borderwidth = 2, relief = GROOVE, bg = "white")
+        self.ecran.pack(expand = TRUE)
+        Label(self.ecran, text = 'Paramètres', font = '-size 50', bg = 'white').grid(row = 0, column = 0, columnspan = 5, padx = 100, pady = 50)
+        Label(self.ecran, text = 'Points gagnants :', font = '-size 15', bg = 'white').grid(row = 1, column = 2)
+        self.e_win = Entry(self.ecran, textvariable = var_win, width = 10, relief = GROOVE, bd = 3)
+        self.e_win.grid(row = 1, column = 3, ipadx = 20, pady = 20)
+        Label(self.ecran, text = 'Vitesse de la balle (en px) :', font = '-size 15', bg = 'white').grid(row = 2, column = 2)
+        self.e_vit = Entry(self.ecran, textvariable = var_vit, width = 10, relief = GROOVE, bd = 3)
+        self.e_vit.grid(row = 2, column = 3, ipadx = 20, pady = 20)
+        Button(self.ecran, text = 'OK', command = start_pong, font = '-size 22').grid(row = 4, column = 4, ipadx = 20, pady = 20)
 
 class Canevas: # start_pong
 
@@ -28,14 +44,15 @@ class Balle: # start_pong
     def __init__(self):
         self.balle = canvas.create_oval((can.centre_x - 10, can.centre_y - 10), (can.centre_x + 10, can.centre_y + 10), fill = 'white')
 
-    dx, dy = 3, 3
-
     def gameTick(self):
+        global dx, dy
         coo = canvas.coords(self.balle)
         coo1 = canvas.coords(raq1)
         coo2 = canvas.coords(raq2)
+        dx = int(dx)
+        dy = int(dy)
         if coo[0] <= coo1[0] or coo[2] >= coo2[2]:
-            if Balle.dx < 0:
+            if dx < 0:
                 scores['J2'] += 1
             else:
                 scores['J1'] += 1
@@ -43,15 +60,15 @@ class Balle: # start_pong
             canvas.itemconfigure(score.J2, text = scores['J2'])
             canvas.coords(balle, can.centre_x - 10, can.centre_y - 10, can.centre_x + 10, can.centre_y + 10)
             fenetre.after(1000, self.gameTick)
-        elif scores['J1'] == 3 or scores['J2'] == 3:
+        elif scores['J1'] == int(win) or scores['J2'] == int(win):
             victoire()
         else:
             if len(canvas.find_overlapping(coo1[0], coo1[1], coo1[2], coo1[3])) > 1 \
             or len(canvas.find_overlapping(coo2[0], coo2[1], coo2[2], coo2[3])) > 1:
-                Balle.dx *= -1
+                dx *= -1
             elif coo[1] <= 0 or coo[3] >= can.hauteur:
-                Balle.dy *= -1
-            canvas.move(balle, self.dx, self.dy)
+                dy *= -1
+            canvas.move(balle, dx, dy)
             fenetre.after(10, self.gameTick)
 
 
@@ -72,18 +89,31 @@ class Score(): # start_pong
 
 
 # Fonctions
+def parametrage():
+    global ecr, ecran
+    menu.destroy()
+    ecr = EcranParametre()
+    ecran = ecr.ecran 
+
 def start_pong(): # menu
-        global can, canvas, bal, balle, raq, raq1, raq2, score, scores
-        menu.destroy()
-        can = Canevas()
-        canvas = can.canvas
-        bal = Balle()
-        balle = bal.balle
-        raq = Raquettes()
-        raq1, raq2 = raq.raq1, raq.raq2
-        score = Score()
-        scores = {'J1' : 0, 'J2' : 0}
-        bal.gameTick()
+    global can, canvas, bal, balle, raq, raq1, raq2, score, scores, win, dx, dy
+    win = ecr.e_win.get()
+    if win == '':
+        win = 3
+    dx = ecr.e_vit.get()
+    if dx == '':
+        dx = 3
+    dy = dx
+    ecran.destroy()
+    can = Canevas()
+    canvas = can.canvas
+    bal = Balle()
+    balle = bal.balle
+    raq = Raquettes()
+    raq1, raq2 = raq.raq1, raq.raq2
+    score = Score()
+    scores = {'J1' : 0, 'J2' : 0}
+    bal.gameTick()
 
 def move(event): # clavier
     coo1 = canvas.coords(raq1)
